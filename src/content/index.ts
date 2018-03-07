a1229794541e6ee503cea4ed68263b6aae4f69d9
atif.afzal <atif.afzal@paytm.com>
2018-03-07 16:12:53 +0530

    inject mimic

--- src/content/index.ts
+++ src/content/index.ts
@@ -2,11 +2,9 @@ const s = document.createElement('script');
 
 const src = chrome.extension.getURL('inject.bundle.js');
 
-console.log(src);
-
 s.src = src;
 s.onload = function() {
   this.remove();
 };
 
-document.head.appendChild(s);
+document.documentElement.appendChild(s);
